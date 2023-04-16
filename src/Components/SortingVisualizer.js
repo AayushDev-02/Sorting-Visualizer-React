b05ce288b0a9ebcfd44b1b325deb502828cd6ecd
Aayush Yadav <yadavaayush002@gmail.com>
2023-04-16 22:15:08 +0530

    Added base changes

--- src/Components/SortingVisualizer.js
+++ src/Components/SortingVisualizer.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function SortingVisualizer() {
-  return (
-    <div>
-      
-    </div>
-  )
+export class SortingVisualizer extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            array: []
+        }
+    }
+
+    componentDidMount(){
+
+    }
+  render() {
+
+    // const {array} = this.state; 
+    const array = [4,3,2,5,1];
+    
+    return (
+      <div>
+        {array.map((value, index) => (
+            <div key={index} className={`h-[${value}px] w-[2px] inline-block mx-1`}>
+            </div>
+        ))}
+
+        <button onClick={() => this.resetArray()}></button>
+      </div>
+    )
+  }
 }
 
 export default SortingVisualizer
