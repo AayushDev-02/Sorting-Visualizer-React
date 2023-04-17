2b350a8fb438982498699cac74918cf3d8f7a488
Aayush Yadav <yadavaayush002@gmail.com>
2023-04-17 21:32:41 +0530

    Made animations for merge sort done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { getMergeSortAnimation } from './Components/SortingAlgo';
+import { getMergeSortAnimations } from './Components/SortingAlgo';
 
 const numberOfArrayBars = 80;
 const minValue = 10;
 const maxValue = 600;
 const PRIMARY_COLOR = "#B2A4FF"
-const SECONDARY_COLOR = "#FFB4B4"
-const ANIMAITON_SPEED = 10
+const SECONDARY_COLOR = "#FFDEB4"
+const ANIMAITON_SPEED = 20
 
 function App() {
   const startArr = [];
@@ -22,13 +22,13 @@ function App() {
   }
 
   const mergeSort = () => {
-    const animation = getMergeSortAnimation(array);
+    const animation = getMergeSortAnimations(array);
     for(let i = 0; i< animation.length; i++){
-      const arrayBars = document.querySelector('#bar');
+      const arrayBars = document.getElementsByClassName('array-bar');
       const isColorChange = i%3 !== 2;
 
       if(isColorChange){
-        const [barOneIndex, barTwoIndex ] = animation[1];
+        const [barOneIndex, barTwoIndex ] = animation[i];
         const barOneStyle = arrayBars[barOneIndex].style;
         const barTwoStyle = arrayBars[barTwoIndex].style;
         const color = i%3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR
@@ -38,6 +38,12 @@ function App() {
           barTwoStyle.backgroundColor = color
         }, i*ANIMAITON_SPEED);
 
+      }else{
+        setTimeout(() => {
+          const [barOneIndex, newHeight] = animation[i];
+          const barOneStyle = arrayBars[barOneIndex].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i*ANIMAITON_SPEED)
       }
     }
   }
@@ -46,7 +52,7 @@ function App() {
     const arr = [];
     for (let i = 0; i < numberOfArrayBars; i++) {
       arr.push(randomIntFromInterval(minValue, maxValue));  
-    }
+    } 
 
     setArray(arr);
   }
@@ -56,7 +62,7 @@ function App() {
       <div className="px-10 py-5 bg-red-100 justify-between flex">
         <button onClick={renderArray} className="w-fit bg-pink-500 text-white font-bold px-3 py-2  bg-red rounded ">Generate Array</button>
         <div className="space-x-5">
-          <button onClick={mergeSort} className="w-fit bg-pink-500 text-white font-bold px-3 py-2  bg-red rounded ">Merge Sort</button>
+          <button id="merge-sort" onClick={mergeSort} className="w-fit bg-pink-500 text-white font-bold px-3 py-2  bg-red rounded ">Merge Sort</button>
           <button onClick={mergeSort} className="w-fit bg-pink-500 text-white font-bold px-3 py-2  bg-red rounded ">Quick Sort</button>
           <button onClick={mergeSort} className="w-fit bg-pink-500 text-white font-bold px-3 py-2  bg-red rounded ">Bubble Sort</button>
         </div>
@@ -66,7 +72,7 @@ function App() {
 
           {array.map((value, index) => (
 
-            <div  key={index} style={{ height: value, backgroundColor: PRIMARY_COLOR }} className={`w-[10px] rounded bar `}>
+            <div  key={index} style={{ height: value, backgroundColor: PRIMARY_COLOR }} className={`w-[10px] rounded array-bar shadow-xl `}>
               {/* {console.log(value)} */}
             </div>
           ))}
--- src/Components/SortingAlgo.js
+++ src/Components/SortingAlgo.js
@@ -1,53 +1,77 @@
-export const getMergeSortAnimation = (array) =>{
-    const animation = [];
-    if(array.length <= 1){
-        return array;
-    }
-
+export function getMergeSortAnimations(array) {
+    const animations = [];
+    if (array.length <= 1) return array;
     const auxiliaryArray = array.slice();
-    mergeSortHelper(array,0,array.length-1,auxiliaryArray,animation);
-    return animation;
-}
-
-const mergeSortHelper = (array, startIndex, endIndex, auxiliaryArray, animation) =>{
-
-    if(startIndex === endIndex) return;
-
-    const middleIndex = Math.floor(startIndex + (endIndex - startIndex) / 2);
-
-    mergeSortHelper(array, startIndex, middleIndex, auxiliaryArray, animation);
-    mergeSortHelper(array, middleIndex + 1, endIndex, auxiliaryArray, animation);
-    doMerge(array, startIndex, middleIndex, endIndex, auxiliaryArray, animation);
-
-}
-
-const doMerge = (array, startIndex, middleIndex, endIndex, auxiliaryArray, animation) =>{
-    let k = startIndex;
-    let i = startIndex;
-    let j = middleIndex + 1;
-    while(i <= middleIndex && j <= endIndex){
-        animation.push([i,j]);
-        animation.push([i,j]);
-        if(auxiliaryArray[i] <= auxiliaryArray[j]){
-            animation.push([k, auxiliaryArray[i]])
-            array[k++] = auxiliaryArray[i++];
-        }
-        else{
-            animation.push([k,auxiliaryArray[j]]);
-            array[k++] = auxiliaryArray[j++];
-        }
-
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+    return animations;
+  }
+  
+  function mergeSortHelper(
+    mainArray,
+    startIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ) {
+    if (startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+  }
+  
+  function doMerge(
+    mainArray,
+    startIdx,
+    middleIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ) {
+    let k = startIdx;
+    let i = startIdx;
+    let j = middleIdx + 1;
+    while (i <= middleIdx && j <= endIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([i, j]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([i, j]);
+      if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+        // We overwrite the value at index k in the original array with the
+        // value at index i in the auxiliary array.
+        animations.push([k, auxiliaryArray[i]]);
+        mainArray[k++] = auxiliaryArray[i++];
+      } else {
+        // We overwrite the value at index k in the original array with the
+        // value at index j in the auxiliary array.
+        animations.push([k, auxiliaryArray[j]]);
+        mainArray[k++] = auxiliaryArray[j++];
+      }
     }
-
-    while( i <= middleIndex){
-        animation.push([k,auxiliaryArray[i]]);
-        array[k++] = auxiliaryArray[i++];
+    while (i <= middleIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([i, i]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([i, i]);
+      // We overwrite the value at index k in the original array with the
+      // value at index i in the auxiliary array.
+      animations.push([k, auxiliaryArray[i]]);
+      mainArray[k++] = auxiliaryArray[i++];
     }
-    while(j<=endIndex) {
-        animation.push([j,j])
-        animation.push([j,j])
-
-        animation.push([k, auxiliaryArray[j]]);
-        array[k++] = auxiliaryArray[j++];
+    while (j <= endIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([j, j]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([j, j]);
+      // We overwrite the value at index k in the original array with the
+      // value at index j in the auxiliary array.
+      animations.push([k, auxiliaryArray[j]]);
+      mainArray[k++] = auxiliaryArray[j++];
     }
-}
+  }
